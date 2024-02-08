e719e6a140826e07036e7024eb3ab0e67889526b
garrettjash <garrettashcroft@gmail.com>
2024-02-08 10:23:19 -0700

    Js file

---
+++ Mission5/wwwroot/js/Calculator.js
@@ -0,0 +1,19 @@
+﻿function Calculate() {
+    let iHours = parseFloat($('#hours').val());
+    console.log("iHours", iHours)
+
+    //Validation to make sure they actually entered a positive number
+    if (iHours < 0) {
+        alert('Try a number more than 0!');
+        $('#hours').val(0);
+    }
+    else {
+        let iRate = parseFloat($('#rate').val());
+        console.log("Rate", iRate)
+
+        let iTotal = iHours * iRate;
+        console.log("iTotal", iTotal)
+
+        $('#total').html(iTotal);
+    }
+}
